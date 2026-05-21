ab341a416e88dfd2500e258368b838705ba09f51
allabakash486 <allabakashushaik19@gmail.com>
2026-05-21 23:44:34 +0530

    new programs added

---
+++ Learning/day_01/number_patterns.py
@@ -0,0 +1,58 @@
+# This methode will verify whether the number is even or odd.
+def even_or_odd(num):
+    if num % 2 == 0:
+        return "even"
+    return "odd"
+number = 10
+result = even_or_odd(number)
+print("The number", number, "is", result)
+
+#This number will print whether the number is positive, negative or zero.
+def positive_negative_or_zero(num):
+    if num > 0:
+        return "positive"
+    elif num < 0:
+        return "negative"
+    return "zero"
+number = int(input("Enter a number: "))
+result = positive_negative_or_zero(number)
+print("The number", number, "is", result)
+
+
+#this program will print whether the number is prime or not.
+def prim_or_not(num):
+    if num <= 1:
+        return "not prime"
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return "not prime"
+    return "prime"
+number = int(input("Enter a number: ")) 
+result = prim_or_not(number)
+print("The number", number, "is", result)
+
+
+#this program will print whether the number is a perfect square or not.
+def perfect_square_or_not(num):
+    if num < 0:
+        return "not a perfect square"
+    root = int(num**0.5)
+    if root * root == num:
+        return "perfect square"
+    return "not a perfect square"
+number = int(input("Enter a number: "))
+result = perfect_square_or_not(number)
+print("The number", number, "is", result)
+
+#this program will print whether the number is a perfect cube or not.
+def perfect_cube_or_not(num):
+    if num < 0:
+        return "not a perfect cube"
+    root = int(num ** (1/3))
+    if root * root * root == num:
+        return "perfect cube"
+    return "not a perfect cube"
+number = int(input("Enter a number: "))
+result = perfect_cube_or_not(number)
+print("The number", number, "is", result)
+
